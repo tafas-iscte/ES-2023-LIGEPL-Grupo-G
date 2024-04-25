87eb0e7721ca01c5a987240fb96bbfe548f94257
tiago <tiago@TiagoLopes>
2024-04-25 13:52:35 +0100

    horariosSala

---
+++ gestao_de_horarios/src/main/java/pt/iscte/gestao_de_horarios/Sala.java
@@ -0,0 +1,123 @@
+package pt.iscte.gestao_de_horarios;
+
+public class Sala {
+	
+	private String edificio;
+	private String nomesala;
+	private String capacidadenormal;
+	private String capacidadeexame;
+	private String numerocaracteristicas;
+	private String anfiteatroaulas;
+	private String apoiotecnicoeventos;
+	private String arq1;
+	private String arq2;
+	private String arq3;
+	private String arq4;
+	private String arq5;
+	private String arq6;
+	private String arq9;
+	private String byod;
+	private String focusgroup;
+	private String horariossalaportalpublico;
+	private String laboratorioarq1;
+	private String laboratorioarq2;
+	private String laboratorioeng;
+	private String laboratorioeletro;
+	private String laboratorioinfor;
+	private String laboratoriojorna;
+	private String laboratorioredes1;
+	private String laboratorioredes2;
+	private String laboratoriotelec;
+	private String mestrado;
+	private String mestradoplus;
+	private String nee;
+	private String provas;
+	private String reunioes;
+	private String arquitetura;
+	private String normal;
+	private String videoconferencia;
+	private String trio;
+	
+	
+	
+	public Sala(String edificio,
+	 String nomesala,
+	 String capacidadenormal,
+	 String capacidadeexame,
+	 String numerocaracteristicas,
+	 String anfiteatroaulas,
+	 String apoiotecnicoeventos,
+	 String arq1,
+	 String arq2,
+	 String arq3,
+	 String arq4,
+	 String arq5,
+	 String arq6,
+	 String arq9,
+	 String byod,
+	 String focusgroup,
+	 String horariossalaportalpublico,
+	 String laboratorioarq1,
+	 String laboratorioarq2,
+	 String laboratorioeng,
+	 String laboratorioeletro,
+	 String laboratorioinfor,
+	 String laboratoriojorna,
+	 String laboratorioredes1,
+	 String laboratorioredes2,
+	 String laboratoriotelec,
+	 String mestrado,
+	 String mestradoplus,
+	 String nee,
+	 String provas,
+	 String reunioes,
+	 String arquitetura,
+	 String normal,
+	 String videoconferencia,
+	 String trio) {
+		
+		
+		 this.edificio = edificio;
+		 this.nomesala = nomesala;
+		 this.capacidadenormal = capacidadenormal;
+		 this.capacidadeexame = capacidadeexame;
+		 this.numerocaracteristicas = numerocaracteristicas;
+		 this.anfiteatroaulas = anfiteatroaulas;
+		 this.apoiotecnicoeventos = apoiotecnicoeventos;
+		 this.arq1 = arq1;
+		 this.arq2 = arq2;
+		 this.arq3 = arq3;
+		 this.arq4 = arq4;
+		 this.arq5 = arq5;
+		 this.arq6 = arq6;
+		 this.arq9 = arq9;
+		 this.byod = byod;
+		 this.focusgroup = focusgroup;
+		 this.horariossalaportalpublico = horariossalaportalpublico;
+		 this.laboratorioarq1 = laboratorioarq1;
+		 this.laboratorioarq2 = laboratorioarq2;
+		 this.laboratorioeng = laboratorioeng;
+		 this.laboratorioeletro = laboratorioeletro;
+		 this.laboratorioinfor = laboratorioinfor;
+		 this.laboratoriojorna = laboratoriojorna;
+		 this.laboratorioredes1 = laboratorioredes1;
+		 this.laboratorioredes2 = laboratorioredes2;
+		 this.laboratoriotelec = laboratoriotelec;
+		 this.mestrado = mestrado;
+		 this.mestradoplus = mestradoplus;
+		 this.nee = nee;
+		 this.provas = provas;
+		 this.reunioes = reunioes;
+		 this.arquitetura = arquitetura;
+		 this.normal = normal;
+		 this.videoconferencia = videoconferencia;
+		 this.trio = trio;
+		 
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ gestao_de_horarios/src/main/java/pt/iscte/gestao_de_horarios/SalasReader.java
@@ -0,0 +1,102 @@
+package pt.iscte.gestao_de_horarios;
+
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class SalasReader {
+	
+	    public static List<Sala> carregarSalas(File file) {
+	        List<Sala> salas = new ArrayList<>();
+	        String linha;
+	        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+	            while ((linha = br.readLine()) != null) {
+	            	
+	                String[] dados = linha.split(";"); // Assume que a vírgula é o delimitador
+	                
+	                String[] dataWorked = new String[35];
+	                
+	                for(int i = 0; i < dados.length; i++) {
+	                	dataWorked[i] = dados[i];
+	                }
+	               
+	                	Sala sala = new Sala(dataWorked[0], dataWorked[1], dataWorked[2], dataWorked[3], dataWorked[4], dataWorked[5], dataWorked[6], dataWorked[7], dataWorked[8], dataWorked[9], dataWorked[10],dataWorked[11], dataWorked[12], dataWorked[13], dataWorked[14], dataWorked[15], dataWorked[16], dataWorked[17], dataWorked[18], dataWorked[19], dataWorked[20], dataWorked[21],dataWorked[22], dataWorked[23], dataWorked[24], dataWorked[25], dataWorked[26], dataWorked[27], dataWorked[28], dataWorked[29], dataWorked[30], dataWorked[31], dataWorked[32],dataWorked[33], dataWorked[34]);
+	                	salas.add(sala);
+	                
+	            }
+	            
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        
+	        return salas;
+	    }
+	    
+	    
+	    public static void main(String Args[]) {
+	    	 String folderPath = "C:\\Users\\tiago\\git\\ES-2023-LIGEPL-Grupo-G\\gestao_de_horarios\\uploads";
+
+	         // Create a File object representing the folder
+	         File folder = new File(folderPath);
+	         while (true) {
+
+	             // Check if the folder exists
+	             if (folder.exists() && folder.isDirectory()) {
+	                 // Get the list of files and directories inside the folder
+	                 File[] files = folder.listFiles();
+
+	                 // Check if the array is not null and has elements
+	                 if (files != null && files.length > 0) {
+	                     SalasReader file = new SalasReader();
+	                     String jsonFileName = file.convertToJson(files[0]);
+	                 } else {
+	                     System.out.println("The folder is empty.");
+	                 }
+	             } else {
+	                 System.out.println("The specified folder does not exist or is not a directory.");
+	             }
+	         }
+	    }
+	    
+	    private void openBrowserWindow(String url) {
+	        try {
+	            Desktop.getDesktop().browse(java.net.URI.create(url));
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    
+	    
+	    
+	    public String convertToJson(File file) {
+	    	
+	        List<Sala> salas = carregarSalas(file);
+	        // Fazer algo com a lista de horários
+	        
+	       /* for(Horario h: horarios) {
+	        	System.out.println(h);
+	        }*/
+	        
+	        
+	        Gson gson = new Gson();
+	        String salasJson = gson.toJson(salas);
+	        String name = "salas.json";
+	        // Save the JSON string to a file
+	        try (FileWriter fileWriter = new FileWriter("public/" + name)) {
+	            fileWriter.write(salasJson);
+	            System.out.println("escreveu");
+	            return name;
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	    }
+	        return name;
+    }
+}
